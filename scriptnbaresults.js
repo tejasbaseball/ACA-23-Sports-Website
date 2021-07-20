974700f071a3c592492817572f41c554177b37cb
tejasbaseball <55719023+tejasbaseball@users.noreply.github.com>
2021-07-20 13:16:25 -0700

    ACA025 Sports Website

---
+++ scriptnbaresults.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPw2rL_HclI977tLz6J0yvqo8ZYoaNatU",
+    authDomain: "warriors-results.firebaseapp.com",
+    databaseURL: "https://warriors-results-default-rtdb.firebaseio.com",
+    projectId: "warriors-results",
+    storageBucket: "warriors-results.appspot.com",
+    messagingSenderId: "644209350413",
+    appId: "1:644209350413:web:17c37844c5e8fc1cd2c244"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
